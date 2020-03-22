21a7c6a0fd3c374e255dde58b8ee99898beed653
Abhishek K <abhishekakade@gmail.com>
2020-03-22 21:38:15 +0530

    Updates

---
+++ precache-manifest.f85eb35ee0992084eeb72f2995147f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd25da5519c9980a0da3c4e24061e828",
+    "url": "/covid19-india/index.html"
+  },
+  {
+    "revision": "b3cd50674cc634893552",
+    "url": "/covid19-india/static/css/main.40ec4d36.chunk.css"
+  },
+  {
+    "revision": "25925a0c6b2e0e8d9ea2",
+    "url": "/covid19-india/static/js/2.d6812192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-india/static/js/2.d6812192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cd50674cc634893552",
+    "url": "/covid19-india/static/js/main.b122b21d.chunk.js"
+  },
+  {
+    "revision": "9ad757e8ef79a9e7d8fb",
+    "url": "/covid19-india/static/js/runtime-main.2d360fbf.js"
+  }
+]);
